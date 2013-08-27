df27d10cf316c7718b005418ef9631d4e5188877
ims <ims21@users.sourceforge.net>
2013-08-27 20:57:50 +0200

    refresh text for green button when is moving over founded items and is added timer thanks to Dima73
    Signed-off-by: Littlesat <littlesat99@yahoo.com>

--- epgsearch/src/EPGSearch.py
+++ epgsearch/src/EPGSearch.py
@@ -241,6 +241,7 @@ class EPGSearch(EPGSelection):
 	def onSelectionChanged(self):
 		self["Service"].newService(eServiceReference(str(self["list"].getCurrent()[1])))
 		self["Event"].newEvent(self["list"].getCurrent()[0])
+		EPGSelection.onSelectionChanged(self)
 
 	def closeScreen(self):
 		# Save our history
