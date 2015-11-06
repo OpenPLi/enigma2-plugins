4476bda827f611643cd567d259d2c81463e22bd5
Dima73 <Dima-73@inbox.lv>
2015-11-06 23:09:09 +0200

    AutoTimer: New config option "Remove not existing events"
    -If enabled the autotimer will check the event id (eit) and remove the
    timer if the event not exists in the EPG
    -SeriesPlugin clean up
    Remove import renameTimer - it is deprecated
    Extract name, description only if a timer is returned from
    getSeasonAndEpisode
    Do not overwrite the description it will overwrite the series
    information

--- autotimer/src/AutoTimerSettings.py
+++ autotimer/src/AutoTimerSettings.py
@@ -77,6 +77,7 @@ class AutoTimerSettings(Screen, ConfigListScreen):
 				getConfigListEntry(_("Style auto timers list"), config.plugins.autotimer.style_autotimerslist, _("If the style is advanced, you will see more information about each auto timer.")),
 				getConfigListEntry(_("Skip poll during epg refresh"), config.plugins.autotimer.skip_during_epgrefresh, _("If enabled, the polling will be skipped if EPGRefresh is currently running.")),
 				getConfigListEntry(_("Popup timeout in seconds"), config.plugins.autotimer.popup_timeout, _("If 0, the popup will remain open.")),
+				getConfigListEntry(_("Remove not existing events"), config.plugins.autotimer.check_eit_and_remove, _("Check the event id (eit) and remove the timer if event not exists in EPG.")),
 			],
 			session = session,
 			on_change = self.changed
--- autotimer/src/__init__.py
+++ autotimer/src/__init__.py
@@ -28,6 +28,7 @@ config.plugins.autotimer.editdelay = ConfigNumber(default=3)
 config.plugins.autotimer.interval = ConfigNumber(default=12)
 config.plugins.autotimer.timeout = ConfigNumber(default=1)
 config.plugins.autotimer.popup_timeout = ConfigNumber(default=5)
+config.plugins.autotimer.check_eit_and_remove = ConfigEnableDisable(default=False)
 config.plugins.autotimer.refresh = ConfigSelection(choices=[
 		("none", _("None")),
 		("auto", _("Only AutoTimers created during this session")),
