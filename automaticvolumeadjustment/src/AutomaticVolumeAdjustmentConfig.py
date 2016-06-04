5719efbc5cdfdfe36abd77b3c2707fc56f5186d8
Dima73 <Dima-73@inbox.lv>
2016-06-04 20:38:08 +0300

    [automaticvolumeadjustment] change the storage location on the config file /etc/enigma2/
    *don't use plugin folder for save config file

--- automaticvolumeadjustment/src/AutomaticVolumeAdjustmentConfig.py
+++ automaticvolumeadjustment/src/AutomaticVolumeAdjustmentConfig.py
@@ -25,11 +25,12 @@ from . import _
 
 from Components.config import ConfigSubsection, ConfigText, \
 	config, ConfigInteger, Config, ConfigSubList, ConfigDirectory, NoSave, ConfigYesNo, ConfigSelectionNumber, ConfigSelection
-from os import path as os_path, open as os_open, close as os_close, O_RDWR as os_O_RDWR, O_CREAT  as os_O_CREAT 
+from os import path as os_path, open as os_open, close as os_close, O_RDWR as os_O_RDWR, O_CREAT  as os_O_CREAT
 from pickle import load as pickle_load, dump as pickle_dump
 from enigma import eEnv
 
-CONFIG_FILE_VOLUME = eEnv.resolve('${libdir}/enigma2/python/Plugins/SystemPlugins/AutomaticVolumeAdjustment/config_volume')
+#CONFIG_FILE_VOLUME = eEnv.resolve('${libdir}/enigma2/python/Plugins/SystemPlugins/AutomaticVolumeAdjustment/config_volume')
+CONFIG_FILE_VOLUME = '/etc/enigma2/AutomaticVolumeAdjustment_volume.conf'
 
 def getVolumeDict():
 	if os_path.exists(CONFIG_FILE_VOLUME):
@@ -48,7 +49,8 @@ def saveVolumeDict(dict):
 
 class AutomaticVolumeAdjustmentConfig():
 	def __init__(self):
-		self.CONFIG_FILE = eEnv.resolve('${libdir}/enigma2/python/Plugins/SystemPlugins/AutomaticVolumeAdjustment/config')
+		#self.CONFIG_FILE = eEnv.resolve('${libdir}/enigma2/python/Plugins/SystemPlugins/AutomaticVolumeAdjustment/config')
+		self.CONFIG_FILE = '/etc/enigma2/AutomaticVolumeAdjustment_services.conf'
 		# load config file
 		self.loadConfigFile()
 
