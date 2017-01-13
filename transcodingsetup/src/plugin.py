01383218be9aeba9a3b6900aacd82cc5ef2c2562
Erik Slagter <erik@openpli.org>
2017-01-13 16:45:19 +0100

    transcoding setup: add Ultimo4k as VU+ transcoding model.

--- transcodingsetup/src/plugin.py
+++ transcodingsetup/src/plugin.py
@@ -78,7 +78,7 @@ class TranscodingSetup(ConfigListScreen, Screen):
 		except:
 			pass
 
-		if vumodel == "solo2" or vumodel == "duo2" or vumodel == "solose" or vumodel == "solo4k" or vumodel == "uno4k":
+		if vumodel == "solo2" or vumodel == "duo2" or vumodel == "solose" or vumodel == "solo4k" or vumodel == "uno4k" or vumodel == "ultimo4k":
 			transcoding = "vuplus"
 		else:
 			if boxtype == "et10000" or boxtype == "hd2400":
