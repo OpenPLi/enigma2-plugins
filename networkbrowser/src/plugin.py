bc512119af69e979470ab820cf11708d89258866
Harro Verton <wanwizard@openpli.org>
2018-03-21 10:12:57 +0000

    fixed nl.po; fixed menu layout issue

--- networkbrowser/src/plugin.py
+++ networkbrowser/src/plugin.py
@@ -27,7 +27,7 @@ def MountManagerCallFunction(iface):
 
 def RemountMain(session, iface = None, **kwargs):
 	from AutoMount import iAutoMount
-	iAutoMount.getAutoMountPoints() 
+	iAutoMount.getAutoMountPoints()
 
 def RemountCallFunction(iface):
 	if iNetwork.getAdapterAttribute(iface, "up"):
@@ -37,11 +37,11 @@ def Plugins(path, **kwargs):
 	global plugin_path
 	plugin_path = path
 	return [
-		PluginDescriptor(name=_("NetworkBrowser"), description=_("Search for network shares"), where = PluginDescriptor.WHERE_NETWORKSETUP, fnc={"ifaceSupported": NetworkBrowserCallFunction, "menuEntryName": lambda x: _("NetworkBrowser"), "menuEntryDescription": lambda x: _("Search for network shares...")}),
-		PluginDescriptor(name=_("MountManager"), description=_("Manage network shares"), where = PluginDescriptor.WHERE_NETWORKSETUP, fnc={"ifaceSupported": MountManagerCallFunction, "menuEntryName": lambda x: _("MountManager"), "menuEntryDescription": lambda x: _("Manage your network shares...")}),
-		PluginDescriptor(name=_("Mount again"), description=_("Attempt to mount shares again"), where = PluginDescriptor.WHERE_NETWORKSETUP,
+		PluginDescriptor(name=_("NetworkBrowser"), description=_("Search for network shares")+"\n", where = PluginDescriptor.WHERE_NETWORKSETUP, fnc={"ifaceSupported": NetworkBrowserCallFunction, "menuEntryName": lambda x: _("NetworkBrowser"), "menuEntryDescription": lambda x: _("Search for network shares...")+"\n"}),
+		PluginDescriptor(name=_("MountManager"), description=_("Manage network shares")+"\n", where = PluginDescriptor.WHERE_NETWORKSETUP, fnc={"ifaceSupported": MountManagerCallFunction, "menuEntryName": lambda x: _("MountManager"), "menuEntryDescription": lambda x: _("Manage your network shares...")+"\n"}),
+		PluginDescriptor(name=_("Mount again"), description=_("Attempt to mount shares again")+"\n", where = PluginDescriptor.WHERE_NETWORKSETUP,
 			fnc={"ifaceSupported": RemountCallFunction,
 				"menuEntryName": lambda x: _("Mount again"),
-				"menuEntryDescription": lambda x: _("Attempt to recover lost mounts (in background)")})
+				"menuEntryDescription": lambda x: _("Attempt to recover lost mounts (in background)")+"\n"})
 	]
 
