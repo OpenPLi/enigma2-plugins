388ab44abbbac62c857c4cf1dc3b4862b63e3e4f
Dimitrij <Dima-73@inbox.lv>
2018-09-16 21:56:48 +0300

    [transcodingsetup] restart streamproxy when save config

--- transcodingsetup/src/plugin.py
+++ transcodingsetup/src/plugin.py
@@ -172,7 +172,8 @@ class TranscodingSetup(ConfigListScreen, Screen):
 		if SystemInfo["HasH265Encoder"]:
 			config.plugins.transcodingsetup.vcodec.save()
 		configfile.save()
-
+		if os.path.exists("/etc/init.d/streamproxy.sh"):
+			os.system("/etc/init.d/streamproxy.sh stop && sleep 2 && /etc/init.d/streamproxy.sh start")
 		self.close()
 
 	def KeyNone(self):
