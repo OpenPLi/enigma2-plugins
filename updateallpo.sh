f4d9fb711c5c55271436769c408a4cdba322014a
Pr2 <pr2git@users.noreply.github.com>
2019-05-22 13:03:14 +0200

    backport some multiOS improvements into updateallpo (#521)

--- updateallpo.sh
+++ updateallpo.sh
@@ -3,15 +3,40 @@
 #  
 # Pre-requisite:
 # The following tools must be installed on your system and accessible from path
-# gawk, find, xgettext, gsed, python, msguniq, msgmerge, msgattrib, msgfmt, msginit
+# gawk, find, xgettext, sed, python, msguniq, msgmerge, msgattrib, msgfmt, msginit
 #
 # Run this script from the top folder of enigma2-plugins
 #
 #
 # Author: Pr2 for OpenPLi Team
-# Version: 1.0
+# Version: 1.1
 
 rootpath=$PWD
+localgsed="sed"
+findoptions=""
+
+#
+# Script only run with gsed but on some distro normal sed is already gsed so checking it.
+#
+gsed --version 2> /dev/null | grep -q "GNU"
+if [ $? -eq 0 ]; then
+	localgsed="gsed"
+else
+	"$localgsed" --version | grep -q "GNU"
+	if [ $? -eq 0 ]; then
+		printf "GNU sed found: [%s]\n" $localgsed
+	fi
+fi
+
+#
+# On Mac OSX find option are specific
+#
+if [[ "$OSTYPE" == "darwin"* ]]
+	then
+		# Mac OSX
+		printf "Script running on Mac OSX [%s]\n" "$OSTYPE"
+    	findoptions=" -s -X "
+fi
 #
 # Parsing the folders tree
 #
@@ -21,8 +46,8 @@ for directory in */po/ ; do
 	#
 	# Update Makefile.am to include all existing language files sorted
 	#
-	makelanguages=$(ls *.po | tr "\n" " " | sed 's/.po//g')
-	gsed -i 's/LANGS.*/LANGS = '"$makelanguages"'/' Makefile.am
+	makelanguages=$(ls *.po | tr "\n" " " | $localgsed 's/.po//g')
+	$localgsed -i 's/LANGS.*/LANGS = '"$makelanguages"'/' Makefile.am
 	# git add Makefile.am
     #
 	# Retrieve languages and plugin name from Makefile.am LANGS @ PLUGIN variables for backward compatibility
@@ -35,15 +60,15 @@ for directory in */po/ ; do
 	#
 	# To update only existing files regardless of the defined ones in Makefile.am
 	#
-	# languages=($(ls *.po | tr "\n" " " | gsed 's/.po//g'))
+	# languages=($(ls *.po | tr "\n" " " | $localgsed 's/.po//g'))
 	plugin=$(gawk ' BEGIN { FS=" " } /^PLUGIN/ { print $3 }' Makefile.am)
 	printf "Processing plugin %s\n" $plugin
 	#
 	printf "Creating temporary file $plugin-py.pot\n"
-	find -s -X .. -name "*.py" -exec xgettext --no-wrap -L Python --from-code=UTF-8 -kpgettext:1c,2 --add-comments="TRANSLATORS:" -d $plugin -s -o $plugin-py.pot {} \+
-	gsed --in-place $plugin-py.pot --expression=s/CHARSET/UTF-8/
+	find $findoptions .. -name "*.py" -exec xgettext --no-wrap -L Python --from-code=UTF-8 -kpgettext:1c,2 --add-comments="TRANSLATORS:" -d $plugin -s -o $plugin-py.pot {} \+
+	$localgsed --in-place $plugin-py.pot --expression=s/CHARSET/UTF-8/
 	printf "Creating temporary file $plugin-xml.pot\n"
-	find -s -X .. -name "*.xml" -exec python $rootpath/xml2po.py {} \+ > $plugin-xml.pot
+	find $findoptions .. -name "*.xml" -exec python $rootpath/xml2po.py {} \+ > $plugin-xml.pot
 	printf "Merging pot files to create: %s.pot\n" $plugin
 	cat $plugin-py.pot $plugin-xml.pot | msguniq --no-wrap --no-location -o $plugin.pot -
 	rm $plugin-py.pot $plugin-xml.pot
