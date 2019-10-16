42242bc49c47dfddd60d180306ca7fe482ecf2aa
nautilus7 <spyros.n7@gmail.com>
2019-10-16 16:13:17 +0300

    [audiosync] Use class already defined in enigma2
    HelpableNumberActionMap class is defined in enigma2, so there is no need to redefine it in the plugin.

--- audiosync/src/AC3main.py
+++ audiosync/src/AC3main.py
@@ -1,7 +1,7 @@
 from AC3utils import AC3, PCM, AC3GLOB, PCMGLOB, AC3PCM, SKIN
 from AC3delay import AC3delay
 from enigma import ePoint
-from HelpableNumberActionMap import HelpableNumberActionMap
+from Components.ActionMap import HelpableNumberActionMap
 from Components.Label import Label,MultiColorLabel
 from Components.Pixmap import MultiPixmap
 from Components.ProgressBar import ProgressBar
