8429fac92bc4d9fffd6e721c33f84255ab7290f0
Dimitrij <Dima-73@inbox.lv>
2020-09-21 07:41:54 +0300

    [autotimer] ver. 4.6.5 - changed match search logic
    Now matches without a short/extended description will also be considered
    for adding, duplicate timers are likely to appear.
    But this is the price for not missing anything.
    Attention!A new option has appeared "AutoTimer Settings" --> "Allow
    double timer for different services".
    Further full testing is needed.

--- autotimer/src/plugin.py
+++ autotimer/src/plugin.py
@@ -38,7 +38,7 @@ from AutoTimer import AutoTimer
 autotimer = AutoTimer()
 autopoller = None
 
-AUTOTIMER_VERSION = "4.6.4"
+AUTOTIMER_VERSION = "4.6.5"
 
 try:
 	from Plugins.SystemPlugins.MPHelp import registerHelp, XMLHelpReader
