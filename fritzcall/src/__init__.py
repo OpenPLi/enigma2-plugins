a25ea213a46efa1beb3cc1bf64ec4e04b8301591
Dimitrij <Dima-73@inbox.lv>
2021-07-05 16:09:59 +0300

    [FritzCall] Major UPD: make plugin Python2 and Python3 compatible (#668)
    Author: DrMichael
    Co-authored-by: dhwz <dhwz@gmx.net>
    
    Co-authored-by: dhwz <dhwz@gmx.net>

--- fritzcall/src/__init__.py
+++ fritzcall/src/__init__.py
@@ -2,25 +2,36 @@
 '''
 general functions for FritzCall plugin
 
-$Id: __init__.py 1296 2016-05-02 13:52:11Z michael $
+$Id: __init__.py 1589 2021-04-25 09:48:00Z michael $
 $Author: michael $
-$Revision: 1296 $
-$Date: 2016-05-02 15:52:11 +0200 (Mon, 02 May 2016) $
+$Revision: 1589 $
+$Date: 2021-04-25 11:48:00 +0200 (Sun, 25 Apr 2021) $
 '''
+
+from __future__ import division
+import gettext
+import os
+from logging import NOTSET
+from six.moves import range
 
 from Components.config import config #@UnresolvedImport
 from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_LANGUAGE, SCOPE_PLUGINS, SCOPE_SKIN_IMAGE #@UnresolvedImport
-import gettext
-import os
 from enigma import eBackgroundFileEraser
-from logging import NOTSET
 
 lang = language.getLanguage()
 os.environ["LANGUAGE"] = lang[:2]
 gettext.bindtextdomain("enigma2", resolveFilename(SCOPE_LANGUAGE))
 gettext.textdomain("enigma2")
 gettext.bindtextdomain("FritzCall", "%s%s" % (resolveFilename(SCOPE_PLUGINS), "Extensions/FritzCall/locale/"))
+
+import logging
+logger = logging.getLogger("FritzCall.__init__")
+debug = logger.debug
+info = logger.info
+warning = logger.warning
+error = logger.error
+exception = logger.exception
 
 
 def _(txt): # pylint: disable=C0103
@@ -32,7 +43,7 @@ def _(txt): # pylint: disable=C0103
 # scramble text
 
 
-def __(text, front=True):
+def __(text, front=True):  #pylint: disable=unused-argument
 	#===========================================================================
 	# if len(text) > 5:
 	#	if front:
@@ -42,9 +53,14 @@ def __(text, front=True):
 	# else:
 	#	return '.....'
 	#===========================================================================
+	# debug("__ 1")
 	out = ""
-	for i in range(len(text) / 2):
+	# debug("__ 2")
+	for i in range(len(text) // 2):
+		# debug("__ 3")
 		out = out + text[i * 2] + '.'
+		# debug("__ 4: %s", out)
+	# debug("__ 5")
 	return out
 
 
@@ -60,7 +76,7 @@ def normalizePhoneNumber(intNo):
 	if found:
 		intNo = '00' + found.group(1)
 	intNo = intNo.replace('(', '').replace(')', '').replace(' ', '').replace('/', '').replace('-', '')
-	found = re.match('^49(.*)', intNo) # this is most probably an error
+	found = re.match(r'^49(.*)', intNo) # this is most probably an error
 	if found:
 		intNo = '0' + found.group(1)
 	found = re.match('.*?([0-9]+)', intNo)
