5bcf4360e986fb6b3860ca037bd8444cdfd8eca2
ims <ims21@users.sourceforge.net>
2021-08-11 10:04:21 +0200

    EPGRefresh - ngettext to overview remaining services to scan - fixed count - update .pot/.po files

--- epgrefresh/src/__init__.py
+++ epgrefresh/src/__init__.py
@@ -17,6 +17,11 @@ def _(txt):
 		t = gettext.gettext(txt)
 	return t
 
+def ngettext(singular, plural, n):
+	t = gettext.dngettext('EPGRefresh', singular, plural, n)
+	if t in (singular, plural):
+		t = gettext.ngettext(singular, plural, n)
+	return t
 
 localeInit()
 language.addCallback(localeInit)
