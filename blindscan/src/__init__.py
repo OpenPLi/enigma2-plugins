519e20a40702f83387e140f276db056244f4e704
Dima73 <dima-73@inbox.lv>
2021-08-29 19:55:28 +0300

    [blindscan] adapt as OE-A version
    -author of the original OE-A code Huevos

--- blindscan/src/__init__.py
+++ blindscan/src/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 from Components.Language import language
-from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS
 import gettext
 
 PluginLanguageDomain = "Blindscan"
@@ -15,7 +16,7 @@ def _(txt):
 	if gettext.dgettext(PluginLanguageDomain, txt):
 		return gettext.dgettext(PluginLanguageDomain, txt)
 	else:
-		#print "[" + PluginLanguageDomain + "] fallback to default translation for " + txt
+		#print("[" + PluginLanguageDomain + "] fallback to default translation for " + txt)
 		return gettext.gettext(txt)
 
 
