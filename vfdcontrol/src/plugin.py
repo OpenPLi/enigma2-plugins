a3e87bfdae6e75ab3531acb866dca00c9731a267
ims <ims21@users.sourceforge.net>
2021-09-20 19:52:01 +0200

    VfdControll - update for ustym4kpro and gbtrio4k

--- vfdcontrol/src/plugin.py
+++ vfdcontrol/src/plugin.py
@@ -13,7 +13,7 @@ import Screens.Standby
 from Tools.HardwareInfo import HardwareInfo
 
 use_oled = False
-if HardwareInfo().get_device_model() in ("formuler3", "formuler4", "s1", "h3", "h4", "h5", "lc"):
+if HardwareInfo().get_device_model() in ("formuler3", "formuler4", "s1", "h3", "h4", "h5", "lc", "gbtrio4k", "ustym4kpro"):
 	use_oled = True
 
 config.plugins.VFD_ini = ConfigSubsection()
