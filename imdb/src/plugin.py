c71940a2c302d1055a262433b264ded257b16f92
scriptmelvin <80369196+scriptmelvin@users.noreply.github.com>
2022-05-04 19:54:22 +0200

    Add User-Agent header

--- imdb/src/plugin.py
+++ imdb/src/plugin.py
@@ -87,6 +87,7 @@ class Downloader(object):
 	downloadTimeout = 10  # seconds
 
 	_headers = {
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0',
 		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
 		'Accept-Language': 'en-US,en;q=0.5',
 		'Accept-Encoding': 'gzip, deflate',
