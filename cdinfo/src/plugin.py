cc67419e723b1ebcebefc573043837bb31c06ae3
Hains van den Bosch <hainsvdbosch.ziggo.nl>
2022-08-15 11:42:48 +0200

    cdinfo: Expand sizes to avoid overlap

--- cdinfo/src/plugin.py
+++ cdinfo/src/plugin.py
@@ -21,17 +21,17 @@ config.plugins.CDInfo.CDDB_cache = ConfigYesNo(default=True)
 
 class CDInfo(ConfigListScreen, Screen):
 	skin = """
-		<screen name="CDInfo" position="center,center" size="560,440" title="CDInfo" >
+		<screen name="CDInfo" position="center,center" size="560,455" title="CDInfo" >
 			<ePixmap pixmap="skin_default/buttons/red.png" position="0,0" size="140,40" alphatest="on" />
 			<ePixmap pixmap="skin_default/buttons/green.png" position="140,0" size="140,40" alphatest="on" />
 			<ePixmap pixmap="skin_default/buttons/blue.png" position="420,0" size="140,40" alphatest="on" />
 			<widget source="key_red" render="Label" position="0,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#9f1313" transparent="1" />
 			<widget source="key_green" render="Label" position="140,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
 			<widget source="key_blue" render="Label" position="420,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#18188b" transparent="1" />
-			<widget source="info" render="Label" position="5,50" size="550,40" font="Regular;20" backgroundColor="#25062748" transparent="1" />
+			<widget source="info" render="Label" position="5,50" size="550,50" font="Regular;20" backgroundColor="#25062748" transparent="1" />
 			<widget name="config" position="5,115" size="550,200" scrollbarMode="showOnDemand" />
 			<ePixmap pixmap="skin_default/div-h.png" position="0,330" zPosition="1" size="560,2" />
-			<widget source="info2" render="Label" position="5,350" size="520,80" font="Regular;20" backgroundColor="#25062748" transparent="1" />
+			<widget source="info2" render="Label" position="5,350" size="520,100" font="Regular;20" backgroundColor="#25062748" transparent="1" />
 		</screen>
 		"""
 
