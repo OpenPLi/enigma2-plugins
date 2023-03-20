68cd9959edb6905de1c8debac8bbb065f996c5b9
Dima73 <dima-73@inbox.lv>
2023-03-20 18:35:12 +0200

    [blindscan] update Gigablue UE4K /Quad 4K support GIGA DVB-S2X NIM (TS3L10)
    ATTENTION!
    Searching sometimes results in a total freeze of the receiver.

--- blindscan/src/plugin.py
+++ blindscan/src/plugin.py
@@ -136,8 +136,8 @@ XML_FILE = None
 # _supportNimType is only used by vuplus hardware
 _supportNimType = {'AVL1208': '', 'AVL6222': '6222_', 'AVL6211': '6211_', 'BCM7356': 'bcm7346_', 'SI2166': 'si2166_'}
 
-# For STBs that support multiple DVB-S tuner models, e.g. Solo 4K.
-_unsupportedNims = ('Vuplus DVB-S NIM(7376 FBC)', 'Vuplus DVB-S NIM(45308X FBC)', 'DVB-S2 NIM(45308 FBC)') # format = nim.description from nimmanager
+# For STBs that support PnP DVB-S/S2 tuner models, e.g. VU+Solo 4K,VU+Ultimo 4K,Gigablue UE/Quad 4K
+_unsupportedNims = ("Vuplus DVB-S NIM(7376 FBC)", "Vuplus DVB-S NIM(45308X FBC)", "DVB-S2 NIM(45308 FBC)", "DVB-S2 NIM(45208 FBC)", "DVB-S2X NIM(45308X FBC)", "DVB-S2 NIM(45308 FBC)") # format = nim.description from nimmanager
 
 # blindscan-s2 supported tuners
 _blindscans2Nims = ('TBS-5925', 'DVBS2BOX', 'M88DS3103')
@@ -315,6 +315,7 @@ class Blindscan(ConfigListScreen, Screen, TransponderFiltering):
 				self.satList.append(None)
 
 		# make config
+		self.getCurrentTuner = None
 		self.createConfig()
 
 		self.frontend = None
@@ -323,7 +324,6 @@ class Blindscan(ConfigListScreen, Screen, TransponderFiltering):
 		self.list = []
 		self.status = ""
 		self.onChangedEntry = []
-		self.getCurrentTuner = None
 		self.blindscan_session = None
 		self.tmpstr = ""
 		self.Sundtek_pol = ""
@@ -491,7 +491,12 @@ class Blindscan(ConfigListScreen, Screen, TransponderFiltering):
 			self.i2c_mapping_table = {0: 2, 1: 3, 2: 1, 3: 0}
 
 	def getNimSocket(self, slot_number):
-		return self.i2c_mapping_table.get(slot_number, -1)
+		bus = self.i2c_mapping_table.get(slot_number, -1)
+		if bus == -1:
+			I2CDevice = nimmanager.getI2CDevice(self.feid)
+			if I2CDevice != None:
+				bus = I2CDevice
+		return bus
 
 	def callbackNone(self, *retval):
 		None
@@ -517,7 +522,7 @@ class Blindscan(ConfigListScreen, Screen, TransponderFiltering):
 		if not self.openFrontend():
 			oldref = self.session.nav.getCurrentlyPlayingServiceReference()
 			stop_current_service = True
-			if oldref and self.getCurrentTuner is not None:
+			if oldref and self.getCurrentTuner != None:
 				if self.feid != self.getCurrentTuner:
 					stop_current_service = False
 			if stop_current_service:
