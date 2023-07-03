dfb683dac4e6413ce0fe712475110dc2eae1d720
Dima73 <dima-73@inbox.lv>
2023-07-03 07:59:21 +0300

    [mediadownloader] adapt py3

--- mediadownloader/src/plugin.py
+++ mediadownloader/src/plugin.py
@@ -24,7 +24,7 @@ def download_file(session, url, to=None, askOpen=False, callback=None,
 	from Components.Scanner import ScanFile
 	file = ScanFile(url, autodetect=False)
 
-	from MediaDownloader import MediaDownloader
+	from .MediaDownloader import MediaDownloader
 	session.open(MediaDownloader, file, askOpen, to, callback)
 
 # Item chosen
@@ -32,7 +32,7 @@ def download_file(session, url, to=None, askOpen=False, callback=None,
 
 def filescan_chosen(session, item):
 	if item:
-		from MediaDownloader import MediaDownloader
+		from .MediaDownloader import MediaDownloader
 
 		session.open(MediaDownloader, item[1], askOpen=True)
 
@@ -65,7 +65,7 @@ def filescan_open(items, session, **kwargs):
 			choices
 		)
 	elif Len:
-		from MediaDownloader import MediaDownloader
+		from .MediaDownloader import MediaDownloader
 
 		session.open(MediaDownloader, items[0], askOpen=True)
 
