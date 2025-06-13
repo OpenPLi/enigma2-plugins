02400661e2ef0751fcea7c056dfc7015bc9b911c
Hains van den Bosch <hainsvdbosch@ziggo.nl>
2025-06-13 13:20:41 +0200

    autotimer: Remove superfluous Webinterface plugin check (#1063)
    /usr/lib/enigma2/python/Plugins/Extensions/WebInterface/plugin.py(c/o) doesn't exist for years.
    Some leftover from old (DMM) webinterface?

--- autotimer/src/plugin.py
+++ autotimer/src/plugin.py
@@ -48,17 +48,6 @@ except Exception as e:
 	autotimerHelp = None
 
 
-def isOriginalWebifInstalled():
-	try:
-		from Tools.Directories import fileExists
-	except:
-		return False
-	pluginpath = eEnv.resolve('${libdir}/enigma2/python/Plugins/Extensions/WebInterface/plugin.py')
-	if fileExists(pluginpath) or fileExists(pluginpath + "o") or fileExists(pluginpath + "c"):
-		return True
-	return False
-
-
 def isOpenWebifInstalled():
 	try:
 		from Tools.Directories import fileExists
@@ -113,15 +102,10 @@ def sessionstart(reason, **kwargs):
 			AutoTimerEPGSelectionInit()
 		except:
 			pass
-		if isOriginalWebifInstalled():
+		if isOpenWebifInstalled():
 			try:
 				from Plugins.Extensions.WebInterface.WebChilds.Toplevel import addExternalChild
-				from Plugins.Extensions.WebInterface.WebChilds.Screenpage import ScreenPage
-				from twisted.web import static
-				from twisted.python import util
-				from WebChilds.UploadResource import UploadResource
-
-				from AutoTimerResource import AutoTimerDoParseResource, \
+				from .AutoTimerResource import AutoTimerDoParseResource, \
 					AutoTimerListAutoTimerResource, AutoTimerAddOrEditAutoTimerResource, \
 					AutoTimerRemoveAutoTimerResource, AutoTimerChangeSettingsResource, \
 					AutoTimerSettingsResource, AutoTimerSimulateResource, AutoTimerTestResource, \
@@ -129,58 +113,18 @@ def sessionstart(reason, **kwargs):
 			except ImportError as ie:
 				pass
 			else:
-				if hasattr(static.File, 'render_GET'):
-					class File(static.File):
-						def render_POST(self, request):
-							return self.render_GET(request)
-				else:
-					File = static.File
-
-				# webapi
 				root = AutoTimerListAutoTimerResource()
-				root.putChild(b'parse', AutoTimerDoParseResource())
-				root.putChild(b'remove', AutoTimerRemoveAutoTimerResource())
-				root.putChild(b'upload_xmlconfiguration', AutoTimerUploadXMLConfigurationAutoTimerResource())
-				root.putChild(b'add_xmltimer', AutoTimerAddXMLAutoTimerResource())
-				root.putChild(b'edit', AutoTimerAddOrEditAutoTimerResource())
-				root.putChild(b'get', AutoTimerSettingsResource())
-				root.putChild(b'set', AutoTimerChangeSettingsResource())
-				root.putChild(b'simulate', AutoTimerSimulateResource())
-				root.putChild(b'test', AutoTimerTestResource())
-				addExternalChild(("autotimer", root, "AutoTimer-Plugin", API_VERSION, False))
-
-				# webgui
-				session = kwargs["session"]
-				root = File(util.sibpath(__file__, "web-data"))
-				root.putChild(b'web', ScreenPage(session, util.sibpath(__file__, "web"), True))
-				root.putChild(b'tmp', File('/tmp'))
-				root.putChild(b'uploadfile', UploadResource(session))
-				addExternalChild(("autotimereditor", root, "AutoTimer", "1", True))
-				doLog("[AutoTimer] Use WebInterface")
-		else:
-			if isOpenWebifInstalled():
-				try:
-					from Plugins.Extensions.WebInterface.WebChilds.Toplevel import addExternalChild
-					from .AutoTimerResource import AutoTimerDoParseResource, \
-						AutoTimerListAutoTimerResource, AutoTimerAddOrEditAutoTimerResource, \
-						AutoTimerRemoveAutoTimerResource, AutoTimerChangeSettingsResource, \
-						AutoTimerSettingsResource, AutoTimerSimulateResource, AutoTimerTestResource, \
-						AutoTimerUploadXMLConfigurationAutoTimerResource, AutoTimerAddXMLAutoTimerResource, API_VERSION
-				except ImportError as ie:
-					pass
-				else:
-					root = AutoTimerListAutoTimerResource()
-					root.putChild(b'parse', AutoTimerDoParseResource())
-					root.putChild(b'remove', AutoTimerRemoveAutoTimerResource())
-					root.putChild(b'upload_xmlconfiguration', AutoTimerUploadXMLConfigurationAutoTimerResource())
-					root.putChild(b'add_xmltimer', AutoTimerAddXMLAutoTimerResource())
-					root.putChild(b'edit', AutoTimerAddOrEditAutoTimerResource())
-					root.putChild(b'get', AutoTimerSettingsResource())
-					root.putChild(b'set', AutoTimerChangeSettingsResource())
-					root.putChild(b'simulate', AutoTimerSimulateResource())
-					root.putChild(b'test', AutoTimerTestResource())
-					addExternalChild(("autotimer", root, "AutoTimer-Plugin", API_VERSION))
-					doLog("[AutoTimer] Use OpenWebif")
+				root.putChild('parse', AutoTimerDoParseResource())
+				root.putChild('remove', AutoTimerRemoveAutoTimerResource())
+				root.putChild('upload_xmlconfiguration', AutoTimerUploadXMLConfigurationAutoTimerResource())
+				root.putChild('add_xmltimer', AutoTimerAddXMLAutoTimerResource())
+				root.putChild('edit', AutoTimerAddOrEditAutoTimerResource())
+				root.putChild('get', AutoTimerSettingsResource())
+				root.putChild('set', AutoTimerChangeSettingsResource())
+				root.putChild('simulate', AutoTimerSimulateResource())
+				root.putChild('test', AutoTimerTestResource())
+				addExternalChild(("autotimer", root, "AutoTimer-Plugin", API_VERSION))
+				doLog("[AutoTimer] Use OpenWebif")
 
 
 base_furtherOptions = None
@@ -404,8 +348,6 @@ def parseEPGCallback(ret):
 			if "\n########## " in searchlog_txt:
 				searchlog_txt = searchlog_txt.split("\n########## ")
 				searchlog_txt = str(searchlog_txt[-1]).split("\n")[2:]
-				# for MessageBox remove lines starts with '     AT:' (timerName and match)
-				searchlog_txt = [line for line in searchlog_txt if not line.startswith('     AT:')]
 				#check count and length of searchlog_entries
 				maxlistcount = 10
 				maxtextlength = 55
