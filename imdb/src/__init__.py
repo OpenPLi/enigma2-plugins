2af3f4eee5b0a0e8ef9d7b8adaf4a1d296b753db
WanWizard <wanwizard@openpli.org>
2026-08-08 18:33:21 +0100

    [imdb] parsing fixes, thanks @adoxa

--- imdb/src/__init__.py
+++ imdb/src/__init__.py
@@ -11,10 +11,22 @@ def localeInit():
 
 
 def _(txt):
-	if gettext.dgettext(PluginLanguageDomain, txt):
-		return gettext.dgettext(PluginLanguageDomain, txt)
-	else:
-		return gettext.gettext(txt)
+	t = gettext.dgettext(PluginLanguageDomain, txt)
+	if t == txt:
+		# print("[%s] fallback to default translation for %s" % (PluginLanguageDomain, txt))
+		t = gettext.gettext(txt)
+	return t
 
 
-language.addCallback(localeInit())
+def ngettext(singular, plural, n):
+	t = gettext.dngettext(PluginLanguageDomain, singular, plural, n)
+	if t in (singular, plural):
+		# print("[%s] fallback to default translation for %s, %s, %d" % (PluginLanguageDomain, singular, plural, n))
+		t = gettext.ngettext(singular, plural, n)
+	return t
+
+
+localeInit()
+language.addCallback(localeInit)
+
+__version__ = "1.2"
